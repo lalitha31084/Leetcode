82150cc7a560c7bed5ff52d7f71b41f8eec8d2d0
lalitha31084 <148032121+lalitha31084@users.noreply.github.com>
2025-08-19 15:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.78%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& t, int k) {
+        // queue<int>q;
+        // for(int i=0;i<t.size();i++){
+        //     q.push(i);
+        // }
+        // int tc=0;
+        // while(!q.empty()){
+        //     int p=q.front();
+        //     q.pop();
+        //     tc++;
+        //     t[p]--;
+        //     if(t[p]>0 )q.push(p);
+        //     if (p== k && t[p] == 0) return tc;
+        // }
+        // return tc;
+        int i=0;
+        int c=0;
+        while(t[k]){
+           
+            
+            if(t[i]>0){
+                    c++;
+                     t[i]--;
+            
+            }
+            i=(i+1)%t.size();
+
+        }
+        return c;
+    } 
+};
