336aa49ccf18be6c03a3236035db8211ab028dd3
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-06-05 15:08:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (94.6%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int l=0,r=n-1;
+        while(l<r){
+            int s=numbers[l]+numbers[r];
+            if(s==target){
+                return {l+1,r+1};
+            }
+            else if(s>target){
+                r--;
+            }
+            else {
+                l++;
+            }
+        }
+        return {-1,-1};
+        
+    }
+};
