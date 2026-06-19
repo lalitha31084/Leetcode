f02ae261e81eed5fd0979a648dc77d0812ef1a40
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-06-19 10:36:54 +0530

    Time: 0 ms (100%), Space: 19.2 MB (52.19%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        c=0
+        h=0
+        for i in gain:
+            c+=i
+            h=max(h,c)
+        return h
+        
+        
