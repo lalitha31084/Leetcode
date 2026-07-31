adc67cc300b09d2f659a14c1cfd5f6e9521a01b5
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-07-31 11:39:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (57.32%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string word;
+        int i=1;
+        while(ss>>word){
+            if(word.find(searchWord)==0)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+};
