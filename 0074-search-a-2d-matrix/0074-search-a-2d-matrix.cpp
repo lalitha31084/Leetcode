ab116b4e49c222cae8d25bc94bf10060971c73e5
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-08-11 14:52:05 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            
-            }
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int l=0,r=(n*m)-1;
+        int len=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int i=mid/n; 
+            int j=mid%n; 
+            if(matrix[i][j]==target)
+                return true;
+            else if(matrix[i][j]>target)
+                r=mid-1;
+            else l=mid+1;
+
         }
         return false;
+       
     }
 };
