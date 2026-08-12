be2736b778812280182d3f9464194c3d04396ee0
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-08-12 10:09:38 +0530

    Time: 0 ms (100%), Space: 23.6 MB (90.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i:nums) mp[i]++;
-
+        int n=nums.size();
+        int m1=0,m2=0;
+        int f1=0,f2=0;
+        for(int i=0;i<n;i++){
+            if(f1>0 && nums[i]==m1){
+                f1++;
+            }
+            else if(f2>0 && nums[i]==m2){
+                f2++;
+            }
+            else if(f1==0){
+                f1=1;
+                m1=nums[i];
+            }
+            else if(f2==0){
+                f2=1;
+                m2=nums[i];
+            }
+            else{
+                f1--;
+                f2--;
+            }
+        }
+        int c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == m1)
+                c1++;
+            else if(nums[i] == m2)
+                c2++;
+        }
         vector<int>res;
-        for(auto i:mp){
-            if(i.second>nums.size()/3) 
-                res.push_back(i.first);
+        if(c1>n/3){
+            res.push_back(m1);
+        }
+        if(c2>n/3){
+            res.push_back(m2);
         }
         return res;
         
