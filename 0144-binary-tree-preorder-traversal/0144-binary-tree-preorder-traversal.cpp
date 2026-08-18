7366b51c4f17eacd6436b4963b373d488dc57c22
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-08-18 09:53:56 +0530

    Time: 3 ms (3.01%), Space: 10.7 MB (94.54%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,16 +11,16 @@
  */
 class Solution {
 public:
-void preorder(TreeNode* root,vector<int>& res)
-{
-    if(root==nullptr)return;
-    res.push_back(root->val);
-    preorder(root->left,res);
-    preorder(root->right,res);
-}
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>res;
         preorder(root,res);
-        return res;  
+        return res;   
+    }
+    void preorder(TreeNode* root,vector<int>&res){
+        if(root==NULL)
+            return;
+        res.push_back(root->val);
+        preorder(root->left,res);
+        preorder(root->right,res);
     }
 };
