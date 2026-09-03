8729e6dfb6bf9ece42ffe6331292668440968065
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-09-03 10:06:39 +0530

    Time: 79 ms (10.99%), Space: 165.9 MB (31.94%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       
+        sort(nums1.begin(),nums1.end());
+        if (nums1[0] % 2 != 0)
+            return true;
+
+        for(int x:nums1){
+            if(x%2!=0) 
+                return false;
+        }
+
+        return true;
+        
+    }
+};
