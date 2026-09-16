478ce5d1b94a96edd421a5be9b0ce3b0c4599831
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-09-16 11:49:08 +0530

    Time: 3 ms (23.77%), Space: 14.9 MB (10.39%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int>res;
+        unordered_map<int,int>mp;
+        for(int i:nums) mp[i]++;
+        for(auto& i:mp){
+            if(i.second==1){
+                res.push_back(i.first);
+            }
+        }
+        return res;
+
+        
+    }
+};
