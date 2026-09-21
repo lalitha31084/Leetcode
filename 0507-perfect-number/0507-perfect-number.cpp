7cebc9d6808cd32816c97474eab3a0cf5fc05512
Beri Lalitha Devi <23a91a6108@aec.edu.in>
2026-09-21 19:31:35 +0530

    Time: 2095 ms (17.46%), Space: 7.7 MB (55.72%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int s=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                s+=i;
+            }
+        }
+        return s==num;
+        
+    }
+};
